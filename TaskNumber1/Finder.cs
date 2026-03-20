d048d699220a62410f1eb24874e940f5e2507f24
Frank Torres <frank.torres@retabet.es>
2026-03-20 13:33:58 +0100

    Task #1 Passing the tests

--- TaskNumber1/Finder.cs
+++ TaskNumber1/Finder.cs
@@ -1,9 +1,36 @@
-﻿namespace TaskNumber1;
+﻿using System.Drawing;
+
+namespace TaskNumber1;
 
 public class Finder
 {
-    public static bool PathFinder(string a)
+    public static bool PathFinder(string maze)
     {
-        throw new NotImplementedException();
+        Point position = new Point(0, 0);
+        string[] rows = maze.Split('\n');
+        Point limits = new Point(rows[0].Length - 1, rows.Length - 1);
+
+        while (InsideLimits(position, limits))
+        {
+            if (CanGoRight(position, rows, limits)) position.X++;
+            else if (CanGoDown(position, rows, limits)) position.Y++;
+            else return false;
+        }
+        return true;
+    }
+
+    private static bool CanGoDown(Point position, string[] rows, Point limits)
+    {
+        return position.Y < limits.Y && rows[position.X][position.Y + 1] == '.';
+    }
+
+    private static bool CanGoRight(Point position, string[] rows, Point limits)
+    {
+        return position.X < limits.X && rows[position.X + 1][position.Y] == '.';
+    }
+
+    private static bool InsideLimits(Point position, Point limits)
+    {
+        return position.X < limits.X || position.Y < limits.Y;
     }
 }
