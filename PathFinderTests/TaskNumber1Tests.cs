ff540e748faba95800625209569b9b3c3f6c09b9
Frank Torres <frank.torres@retabet.es>
2026-03-20 13:17:13 +0100

    added Task #1 - Project and tests

---
+++ PathFinderTests/TaskNumber1Tests.cs
@@ -0,0 +1,52 @@
+﻿using TaskNumber1;
+
+namespace PathFinderTests;
+
+public class TaskNumber1Tests
+{
+    [Fact]
+    public void PathFinder_ReturnsTrue_WhenPathExistsInSimpleGrid()
+    {
+        string area = ".W.\n" +
+                      ".W.\n" +
+                      "...";
+
+        Assert.True(Finder.PathFinder(area));
+    }
+
+    [Fact]
+    public void PathFinder_ReturnsFalse_WhenStartIsBlocked()
+    {
+        string area = ".W.\n" +
+                      ".W.\n" +
+                      "W..";
+
+        Assert.False(Finder.PathFinder(area));
+    }
+
+    [Fact]
+    public void PathFinder_ReturnsTrue_WhenOpenGridHasRoute()
+    {
+        string area = "......\n" +
+                      "......\n" +
+                      "......\n" +
+                      "......\n" +
+                      "......\n" +
+                      "......";
+
+        Assert.True(Finder.PathFinder(area));
+    }
+
+    [Fact]
+    public void PathFinder_ReturnsFalse_WhenExitIsIsolatedByWalls()
+    {
+        string area = "......\n" +
+                      "......\n" +
+                      "......\n" +
+                      "......\n" +
+                      ".....W\n" +
+                      "....W.";
+
+        Assert.False(Finder.PathFinder(area));
+    }
+}
---
+++ TaskNumber1/Finder.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskNumber1;
+
+public class Finder
+{
+    public static bool PathFinder(string a)
+    {
+        throw new NotImplementedException();
+    }
+}
